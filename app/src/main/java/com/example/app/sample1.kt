823715513553db6f2d927b2d5c4511094dcf05ac
LeeDongJun <72503526+djlee1995@users.noreply.github.com>
2021-02-20 18:32:28 +0900

    Kotlin 기본문법(1.함수,2.var과val,3.string템플릿,4.조건식,5.array와list,6.For/Whle,7.Nullable/NonNull)

--- app/src/main/java/com/example/app/sample1.kt
+++ app/src/main/java/com/example/app/sample1.kt
@@ -8,8 +8,10 @@ fun main(){/*
     val lastname="Lee"
     println("My name is ${name + lastname} I'm 24")
     println("this is 2\$입니다")*/
-
-    checkNum(1)
+/*
+    checkNum(1)*/
+    //forAndWhile()
+    nullcheck()
 }
 //1. 함수
 fun helloWorld(){ //return 형식이 없으면 써주는
@@ -80,4 +82,57 @@ fun  array(){
     arraylist.add(120)
     arraylist[0] = 20
 
+}
+//6. For/ While
+fun forAndWhile(){
+    val students = arrayListOf("dongjun","sohee","minji","jenny")
+    for(name in students){
+        println("${name}")
+    }
+    for ((index,name) in students.withIndex()){
+        println("${index+1}번째 학생 ${name}")
+    }
+    var sum = 0
+    for (i in 1..10){
+        sum +=i
+    }
+    /*for (i in 1..10 step 2){
+        sum +=i
+    }
+    for (i in 10 downTo 1){
+        sum +=i
+    }
+*/
+    println(sum)
+
+    var index =0
+    while (index<10){
+        println("current index : ${index}")
+        index ++
+    }
+}
+//7. Nullable / NonNull
+fun nullcheck(){
+    //NPE :Null pointer Exception
+
+    var name : String = "dongjun"
+    var nullName : String? = null
+    var nameInUpperCase = name.toUpperCase()
+    var nullNameInUpperCase = nullName?.toUpperCase()
+
+    // ?:(엘비스 연산자)
+    val lastName :String? = null
+    val fullName = name +" "+ (lastName?: "No lastName")
+    println(fullName)
+
+}
+//!! (확신한 Null이 아닐때 쓰는 거)
+fun  ignorNulls(str :String?){
+    val mNotNull :String = str!!
+    val upper = mNotNull.toUpperCase()
+//let : email이 null이 아니라면 let을 실행
+    val email :String?= "djlee1995@daum.net"
+    email?.let {
+        println("my email is ${email}")
+    }
 }
